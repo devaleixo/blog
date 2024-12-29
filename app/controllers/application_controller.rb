e0650d6758295d91af9bc0e14216ff47f181a5f4
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2024-12-29 15:35:07 -0300

    fixed pundit bug, started setting it up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  include Pundit::Authorization
+
   allow_browser versions: :modern
+
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: :home
+
+  after_action :verify_authorized, unless: :skip_pundit?
+  after_action :verify_policy_scoped, unless: :skip_pundit?
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "Não Autorizado"
+    redirect_to(root_path)
+  end
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
 end
