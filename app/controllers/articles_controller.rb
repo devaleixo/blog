021d79f7fb3e0d778587a6897a33affb57ef5d26
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2024-12-29 18:44:07 -0300

    created articles controller, routes and polices

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  
+    def show
+        authorize @article
+    end
+  
+    def new
+      @article = Article.new
+      authorize @article
+    end
+  
+    def edit
+    end
+  
+    def create
+      @article = Article.new(article_params)
+      @article.user = current_user
+      authorize @article
+      if @article.save
+        redirect_to @article, notice: 'O artigo foi criado com sucesso.'
+      else
+        render :new
+      end
+    end
+  
+    def update
+     authorize @article
+      if @article.update(article_params)
+        redirect_to @article, notice: 'O artigo foi atualizado com sucesso.'
+      else
+        render :edit
+      end
+    end
+  
+    # DELETE /articles/1
+    def destroy
+      authorize @article
+      @article.destroy
+      redirect_to root_path, notice: 'O artigo foi excluído com sucesso.'
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_article
+        @article = Article.find(params[:id])
+      end
+  
+      # Only allow a trusted parameter "white list" through.
+      def article_params
+        params.require(:article).permit(:title, :rich_body)
+      end
+  end
+  
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @articles = Article.order(created_at: :desc).page(params[:page]).per(3)
   end
 end
