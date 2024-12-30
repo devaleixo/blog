6855716dca793a04405e090bc7aac0210fb13465
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2024-12-30 04:37:36 -0300

    article validation: rich_body must have at least 500 carecters

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
   belongs_to :author, class_name: "User"
   
   has_rich_text :rich_body
+  
+  validates :rich_body, length: { minimum: 500, too_short: "Deve ter no mínimo 500 caracteres" }
 end
