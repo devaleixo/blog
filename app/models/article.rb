7e92b36a5c30825499dafad102125a540158f1ad
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2024-12-30 19:40:06 -0300

    added comments model and its association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,11 @@
 class Article < ApplicationRecord
   belongs_to :author, class_name: "User"
   
+  has_many :comments
+
   has_rich_text :rich_body
   has_one_attached :photo
   
+  validates :title, presence: true
   validates :rich_body, length: { minimum: 500, too_short: "Deve ter no mínimo 500 caracteres" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, foreign_key: :author_id
+  has_many :comments, foreign_key: :author_id
 end
