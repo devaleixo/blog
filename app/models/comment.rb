dd85eda89be48da3c8f8bf4d19792c06b4b06afd
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2024-12-30 19:41:24 -0300

    length validation for body and optional user reference

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: "User", optional: true
   belongs_to :article
+
+  validates :body, presence: true
+  validates :body, length: { maximum: 200 }
 end
