ccb5c2517c23d0cae4b4b6e138af74be93747485
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2024-12-31 17:36:37 -0300

    created tags model and migrated

---
+++ app/models/tag.rb
@@ -0,0 +1,3 @@
+class Tag < 
+    validates :title, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_31_021305) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_31_203305) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -70,6 +70,12 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_31_021305) do
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
     t.index ["author_id"], name: "index_comments_on_author_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
