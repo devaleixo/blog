cb86ee2f399f608bc7aec8dee14d34feeeae89cd
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2025-01-06 19:39:16 -0300

    job to create blogs added

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   get "up" => "rails/health#show", as: :rails_health_check
-  
+
   devise_for :users
 
   root "pages#home"
-  
+
   resources :articles, except: :index do
+    collection do
+      get :imports
+      post :import
+    end
     resources :comments, only: :create
     resources :taggings, only: :create
   end
