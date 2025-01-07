df31eb1d88f446bbb3edf7d54a8c644a457e81a5
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2025-01-06 22:23:56 -0300

    added pagination to articles on home page, cool gem, never used it

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @articles = Article.order(created_at: :desc)
+    @articles = Article.order(created_at: :desc).page(params[:page]).per(3)
   end
 end
